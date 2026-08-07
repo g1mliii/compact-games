e41f607889654980d32ab1bfcc0d1c83c892b703
g1mliii <subaigsuri1@gmail.com>
2026-08-07 14:12:36 -0400

    chore: prepare v0.2.3 release

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,7 +1,7 @@
 /// Application-wide constants.
 abstract final class AppConstants {
   static const String appName = 'Compact Games';
-  static const String appVersion = '0.2.2';
+  static const String appVersion = '0.2.3';
 
   /// Identifies who owns delivery of application updates for this build.
   ///
